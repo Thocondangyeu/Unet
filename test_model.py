ca70496faa96f33cf95c846c5da6fe51a7d78d7d
Linh Dang <Thocondangyeu>
2021-01-01 16:56:01 +0700

    Hieunk upload trained model

--- test_model.py
+++ test_model.py
@@ -1,14 +1,18 @@
 from build_model import Unet
 import cv2
 import numpy as np
-model=Unet("model-dsbowl2018-1.h5",(256,256,3))
+model=Unet("model.h5",(256,256,3))
 image=cv2.imread("data_for_unet/test/49/images/49.jpg")
 
-image=cv2.resize(image,(256,256))/255
+image=cv2.resize(image,(256,256))
+image=image
+image=image*1./255
 image_expand=np.zeros((1,256,256,3))
 image_expand[0]=image
 result = model.predict(image_expand)
+result=result>0.55
 print(result.shape)
 cv2.imshow("",(result[0]*255).astype(np.uint8))
 cv2.waitKey()
-print(result[0])
+for i in result[0]:
+    print(i)
