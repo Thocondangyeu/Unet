c4963f7d7f9468bc4e319d5e52e53a5baccc4bd3
Thocondangyeu <72959126+Thocondangyeu@users.noreply.github.com>
2021-01-04 20:58:58 +0700

    Add files via upload
    Build lightwetght model

---
+++ light_model.py
@@ -0,0 +1,42 @@
+import numpy as np 
+import os
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+
+def lightModel(input_height=256,input_width=256):
+    img_input = Input(shape=(input_height,input_width , 3 ))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(img_input)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+
+    up1 = concatenate([UpSampling2D((2, 2))(conv3), conv2], axis=-1)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv4)
+
+    up2 = concatenate([UpSampling2D((2, 2))(conv4), conv1], axis=-1)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(up2)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv5)
+    out = Conv2D( 2, (1, 1) , padding='same')(conv5)
+    model=Model(input=img_input,output=conv5)
+    model.summary()
+
+if __name__ == "__main__":
+    model = lightModel()
